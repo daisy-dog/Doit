5a8e424fd129421aacb33e5087a14e74aa816346
Kevin Wright <thekevmain95@gmail.com>
2017-09-14 10:59:17 -0700

    created new screen to add tasks. still need to connect the widgets with actions.

---
+++ Doit/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Doit
+//
+//  Created by Kevin Wright on 9/14/17.
+//  Copyright © 2017 Kevin Wright. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
